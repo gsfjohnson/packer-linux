bddc3547c0d6e5fa367d36f78a8ff41048a831ad
gfjohnson <gsfjohnson@gmail.com>
2015-09-13 13:29:52 -0700

    update co71-m

--- centos-7.1-minimal/scripts/base.sh
+++ centos-7.1-minimal/scripts/base.sh
@@ -1,3 +1,3 @@
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 yum -y install gcc make gcc-c++ kernel-devel-`uname -r` perl
-
+hostnamectl set-hostname centos71-m-a
--- centos-7.1-minimal/scripts/cleanup.sh
+++ centos-7.1-minimal/scripts/cleanup.sh
@@ -1,6 +1,7 @@
 yum -y erase gtk2 libX11 hicolor-icon-theme avahi freetype bitstream-vera-fonts
 yum -y erase gcc gcc-c++ kernel-develi cpp glibc-devel glibc-headers kernel-headers perl
-yum -y erase centos-logos plymouth-core-libs alsa-firmware alsa-libs linux-firmware aic94xx-firmware libstdc++-devel ppp NetworkManager-libnm
+yum -y erase centos-logos plymouth-core-libs alsa-firmware alsa-libs linux-firmware aic94xx-firmware libstdc++-devel ppp NetworkManager-libnm firewalld
+yum -y erase $(rpm -q kernel | grep -v $(uname -r))
 yum -y clean all
 rm -rf VBoxGuestAdditions_*.iso
 rm -rf /tmp/rubygems-*
