376bce9e9154dabeb7a94ef81af7589f818186a9
gfjohnson <gsfjohnson@gmail.com>
2015-09-13 13:28:15 -0700

    update co67 for minimal with ansible

--- centos-6.7/scripts/cleanup.sh
+++ centos-6.7/scripts/cleanup.sh
@@ -1,4 +1,5 @@
 yum -y erase gtk2 libX11 hicolor-icon-theme avahi freetype bitstream-vera-fonts
+yum -y erase gcc gcc-c++ kernel-develi cpp glibc-devel glibc-headers kernel-headers perl
 yum -y clean all
 rm -rf VBoxGuestAdditions_*.iso
 rm -rf /tmp/rubygems-*
