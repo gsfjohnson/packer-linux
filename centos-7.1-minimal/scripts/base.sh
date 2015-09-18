d7bf692f11085aa774dfc2dcba79d52f6808fcb3
Glen Johnson <gfjohnson@alaska.edu>
2015-09-18 12:24:34 -0800

    no grub timeout; improve yum proxyability

--- centos-7.1-minimal/scripts/base.sh
+++ centos-7.1-minimal/scripts/base.sh
@@ -1,3 +1,5 @@
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 yum -y install gcc make gcc-c++ kernel-devel-`uname -r` perl
 hostnamectl set-hostname centos71-m-a
+sed -i "s/TIMEOUT=5/TIMEOUT=0/" /etc/default/grub
+sed -i -e 's:\#baseurl=:baseurl=:' -e 's:mirrorlist=:\#mirrorlist=:' /etc/yum.repos.d/CentOS-Base.repo
