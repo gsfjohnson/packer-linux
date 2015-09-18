2b31f8136910cad8a426521f3ba5b53ec146c245
Glen Johnson <gfjohnson@alaska.edu>
2015-09-18 13:08:44 -0800

    update hostname; adjust pkg cleaning

--- centos-7.1-minimal/scripts/base.sh
+++ centos-7.1-minimal/scripts/base.sh
@@ -1,5 +1,4 @@
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 yum -y install gcc make gcc-c++ kernel-devel-`uname -r` perl
-hostnamectl set-hostname centos71-m-a
-sed -i "s/TIMEOUT=5/TIMEOUT=0/" /etc/default/grub
+hostnamectl set-hostname centos71-m
 sed -i -e 's:\#baseurl=:baseurl=:' -e 's:mirrorlist=:\#mirrorlist=:' /etc/yum.repos.d/CentOS-Base.repo
