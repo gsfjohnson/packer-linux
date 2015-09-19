829b00d3ea65de87c4f3ba3ddcdf6ec6700902ad
Glen Johnson <gfjohnson@alaska.edu>
2015-09-19 08:38:50 -0800

    ensure epel install; keep centos-logos due to grub2 dep

--- centos-7.1-minimal/scripts/cleanup.sh
+++ centos-7.1-minimal/scripts/cleanup.sh
@@ -1,6 +1,6 @@
 yum -y erase gtk2 libX11 hicolor-icon-theme avahi bitstream-vera-fonts
 yum -y erase gcc gcc-c++ kernel-develi cpp glibc-devel glibc-headers kernel-headers perl
-yum -y erase centos-logos plymouth-core-libs linux-firmware libstdc++-devel ppp NetworkManager-libnm firewalld
+yum -y erase plymouth-core-libs linux-firmware libstdc++-devel ppp NetworkManager-libnm firewalld
 yum -y erase $(rpm -q kernel | grep -v $(uname -r))
 yum -y clean all
 rm -rf VBoxGuestAdditions_*.iso
