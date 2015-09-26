44df5c99414b6b15ccb337860994134c932e225e
Glen Johnson <gsfjohnson@gmail.com>
2015-09-26 15:42:05 -0800

    more minimal

--- centos-6.7/scripts/cleanup.sh
+++ centos-6.7/scripts/cleanup.sh
@@ -1,5 +1,6 @@
 yum -y erase gtk2 libX11 hicolor-icon-theme avahi freetype bitstream-vera-fonts
-yum -y erase gcc gcc-c++ kernel-develi cpp glibc-devel glibc-headers kernel-headers perl
+yum -y erase gcc gcc-c++ kernel-devel kernel-firmware cpp glibc-devel glibc-headers kernel-headers perl
+yum -y erase $(rpm -q kernel | grep -v $(uname -r))
 yum -y clean all
 rm -rf VBoxGuestAdditions_*.iso
 rm -rf /tmp/rubygems-*
