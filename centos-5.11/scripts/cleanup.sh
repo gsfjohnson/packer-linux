c8ea20b51aeba492a26f3e2cb7ec52c7bae3177e
Glen Johnson <gfjohnson@alaska.edu>
2015-10-10 14:34:28 -0800

    tune cleanup

--- centos-5.11/scripts/cleanup.sh
+++ centos-5.11/scripts/cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-yum -y erase bluez-gnome bluez-libs gpm irda-utils cups-libs cpp wireless-tools system-config-securitylevel-tui
+yum -y erase cpp wireless-tools system-config-securitylevel-tui
 yum -y erase $(rpm -qa | grep kernel | grep -v `uname -r`)
 yum -y clean all
 rm -rf *.iso *.iso.?
@@ -9,5 +9,6 @@ rm -rf /tmp/*
 sed -i 's/^HWADDR.*$//' /etc/sysconfig/network-scripts/ifcfg-eth0
 sed -i 's/^UUID.*$//' /etc/sysconfig/network-scripts/ifcfg-eth0
 
+echo Zeroing out unused space...
 dd if=/dev/zero of=/EMPTY bs=1M
 rm -rf /EMPTY
