05988c401534559b29695d8296576d8ce96036c0
Glen Johnson <gfjohnson@alaska.edu>
2015-10-20 10:21:30 -0800

    update box hostname and repo baseurl vs mirrorlist

--- centos-7.1/scripts/base.sh
+++ centos-7.1/scripts/base.sh
@@ -1,4 +1,4 @@
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 yum -y install gcc make gcc-c++ kernel-devel-`uname -r` perl
-hostnamectl set-hostname centos71-m
-sed -i -e 's:\#baseurl=:baseurl=:' -e 's:mirrorlist=:\#mirrorlist=:' /etc/yum.repos.d/CentOS-Base.repo
+hostnamectl set-hostname centos71
+# sed -i -e 's:\#baseurl=:baseurl=:' -e 's:mirrorlist=:\#mirrorlist=:' /etc/yum.repos.d/CentOS-Base.repo
